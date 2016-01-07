686cb48573c90b06b5cb2a7819092ccfd0f13d80
Zak Kent <zak.kent@gmail.com>
2016-01-07 13:57:29 -0800

    hello world 2

---
+++ hello_world2.py
@@ -0,0 +1,11 @@
+#def say_hello():
+	#user_name = input("What's your name: ")
+	#print("hello {}".format(user_name))
+
+#say_hello()
+
+saying = "Hello World"
+
+name = input("What's your name: ")
+
+print(saying, name)
