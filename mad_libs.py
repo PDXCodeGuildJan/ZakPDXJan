efbc6b5298ad442d9e64ff5695ed8f4beb76e257
Zak Kent <zak.kent@gmail.com>
2016-01-07 14:23:56 -0800

    simple mad libs

---
+++ mad_libs.py
@@ -0,0 +1,28 @@
+mad_lib = "Most doctors agree that bicycle "
+
+verb_ing = input("Please give me a verb ending in 'ing' ")
+
+mad_lib += verb_ing + " is a/an "
+
+adjective = input("Please give an adjective: ")
+
+mad_lib += adjective + " of exericise "
+
+verb_ing2 = input("Please give me a verb ending in 'ing': ")
+
+mad_lib += verb_ing2 + " a bicycle enables you to develop your "
+
+part_of_body = input("Please name a part of your body that exercies benefits: ")
+
+mad_lib += part_of_body + " muscles as well as your "
+
+part_of_body2 = input("Please name another part of your body that exercise benefits: ")
+
+mad_lib += part_of_body2 + " That's my mad_lib "
+
+
+
+
+print(mad_lib)
+
+
