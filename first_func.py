84e928220e0e5e4c3a4b385ba43b32a7fadd0f83
Zak Kent <zak.kent@gmail.com>
2016-01-08 13:58:23 -0800

    die roll update

--- first_func.py
+++ first_func.py
@@ -6,17 +6,44 @@ def die():
 
 # rolls custom die 
 def custom_die(low, high):
-	print("Your range was {}, {}".format(str(low), str(high)))
-	print(randint(low, high))
+	output = randint(low, high)
+	if output == high: 
+		print("{} Critical hit".format(output))
+	elif output == low: 
+		print("{} Critical miss".format(output))
+	else: 
+		print(output)	
 
 def main():
-	# Ask user how many dice to rool 
-	total_rolls = int(input("How many dice? "))
-	total_sides = int(input("How many sides does the die have? "))
+	print("Welcome to the die roller! Enter [Q] to quit. ")
 
-	# Roll that many die 
-	for item in range(total_rolls):
-		custom_die(1, total_sides)
+	entree = ""
+
+	while entree != "q": 
+		# Ask user how many dice to rool 
+		entree = input("How many dice? ")
+		if entree == 'q':
+			exit()
+		total_rolls = int(entree)
+
+		entree = input("How many sides does the die have? ")
+		if entree == 'q':
+			exit()
+		total_sides = int(entree)
+
+		# Roll that many die 
+		for item in range(total_rolls):
+			custom_die(1, total_sides)			 
+
+		# Deciding whether or not to run the program again 	
+		# continue_question = input("Would you like to roll again? [Y/N] ").lower()
+		# if continue_question == "y":
+		# 	continue
+		# elif continue_question == "n":
+		# 	break
+		# else: 
+		# 	print("Please type a valid Y or N after next roll")
+		# 	continue
 
 main()
 
